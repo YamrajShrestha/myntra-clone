60345ab89ea639e163ff32f4d6fad0375f38af4f
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-02-28 14:01:34 +0545

    necessary UI transferred from old project

---
+++ client/src/components/BagSummary.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const BagSummary = () => {
+  const bagSummary = {
+    totalItem: 3,
+    totalMRP: 2345,
+    totalDiscount: 999,
+    finalPayment: 3346,
+  };
+  return (
+    <>
+      <div className="bag-summary">
+        <div className="bag-details-container">
+          <div className="price-header">
+            PRICE DETAILS ({bagSummary.totalItem} Items){" "}
+          </div>
+          <div className="price-item">
+            <span className="price-item-tag">Total MRP</span>
+            <span className="price-item-value">Rs {bagSummary.totalMRP}</span>
+          </div>
+          <div className="price-item">
+            <span className="price-item-tag">Discount on MRP</span>
+            <span className="price-item-value priceDetail-base-discount">
+              -Rs {bagSummary.totalDiscount}
+            </span>
+          </div>
+          <div className="price-item">
+            <span className="price-item-tag">Convenience Fee</span>
+            <span className="price-item-value">Rs 99</span>
+          </div>
+          <hr />
+          <div className="price-footer">
+            <span className="price-item-tag">Total Amount</span>
+            <span className="price-item-value">
+              Rs {bagSummary.finalPayment}
+            </span>
+          </div>
+        </div>
+        <button className="btn-place-order">
+          <div className="css-xjhrni">PLACE ORDER</div>
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default BagSummary;
